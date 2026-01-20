98120a1933be6fca2fb624662dc773a738dea217
Chase Starr <chasestarr9@gmail.com>
2026-01-19 21:18:04 -0500

    updated app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoWrapper />
+    </div>
+  );
+}
+
+function TodoWrapper() {
+  const [todo, setTodo] = useState([]);
+  function addTodo(item) {
+    setTodo([...todo, item]);
+  }
+  return (
+    <div>
+      <TodoForm addTodo={addTodo} />
+      <TodoItem />
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [item, setItem] = useState("");
+
+  function handleValue(e) {
+    e.preventDefault();
+    addTodo(item);
+  }
+
+  return (
+    <form onSubmit={handleValue}>
+      <h1>Todo List</h1>
+      <input
+        value={item}
+        type="text"
+        placeholder="Add a new task..."
+        onChange={(e) => setItem(e.target.value)}
+      />
+      <button>add text</button>
+    </form>
+  );
+}
+
+function TodoItem() {
+  return (
+    <div>
+      <h3>Item</h3>
     </div>
   );
 }
